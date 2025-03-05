29aebe6602c0d5d8354521a3694355997be2eefa
5935512009 <Pasut2541@hotmail.com>
2025-03-05 16:11:08 +0700

    update:{import package}

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
 
 app.use('/test', async(req,res)=>{
     res.json("hello world Bay");
 })
 
-PORT = 4001
+PORT = process.env.PORT|| 5001;
 app.listen(PORT,()=>{
     console.log(`server running on localhost ${PORT}`)
 })
